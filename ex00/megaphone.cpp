c0aa215958c9acabee2db4e6d21cf1f1f5d49609
abduraimoff21 <abduraimov21doston@gmail.com>
2023-07-18 05:06:03 +0400

    modified:   ex00/Makefile 	modified:   ex00/megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,10 +6,30 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:24:42 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/18 03:40:54 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/07/18 05:04:31 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
 
-char 
+int main (int argc, char **argv){
+	int i;
+	int j;
+	char c;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				c = std::toupper(argv[i][j]);	
+				std::cout << c;
+			}
+		}
+		std::cout << "\n";
+	}
+	else
+		std::cout << "*  LOUD AND UNBEARABLE FEEDBACK NOISE  * \n";
+	return 0;
+}
