c8d0741c65e71430391a30c1587eec26e9bd878b
abduraimoff21 <abduraimov21doston@gmail.com>
2023-07-19 23:15:00 +0400

    modified:   ex01/Makefile 	modified:   ex01/phonebook.cpp 	modified:   ex01/phonebook.hpp

--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,7 +6,21 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:26:16 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/16 02:26:17 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/07/19 08:01:40 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+class Phonebook {
+	public:
+		int contact;
+};
+
+class Contact {
+	public:
+		int index;
+		std::string fname;
+		std::string lname;
+		std::string nname;
+};
