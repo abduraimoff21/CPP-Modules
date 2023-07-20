650dde30b75f008b0deb25e18201ac8996cf01cf
abduraimoff21 <abduraimov21doston@gmail.com>
2023-07-20 08:02:14 +0400

    modified:   .vscode/settings.json 	modified:   ex01/Makefile 	new file:   ex01/phonebook 	modified:   ex01/phonebook.cpp 	modified:   ex01/phonebook.hpp 	new file:   ex01/phonebook.o

--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,8 +6,17 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:26:05 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/18 22:03:54 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/07/20 07:40:33 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
+
+int main () {
+	std::string command;
+	
+	std::cout << BLUE << "ENTER COMMANDS IN CAPITAL LETTERS" << std::endl;
+	while (getline(std::cin, command)){
+	}
+	return 0;
+}
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,21 +6,20 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:26:16 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/19 08:01:40 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/07/20 07:41:28 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#ifndef PHONENOOK_HPP
+# define PHONENOOK_HPP
 
-class Phonebook {
-	public:
-		int contact;
-};
+# include <iostream>
+# include <string>
+ # define RED		"\033[31m"
+ # define GREEN 	"\033[32m"
+ # define YELLOW	"\033[33m"
+ # define BLUE		"\033[34m"
+ # define MAGENTA	"\033[35m"
+ # define WHITE		"\033[37m"
 
-class Contact {
-	public:
-		int index;
-		std::string fname;
-		std::string lname;
-		std::string nname;
-};
+#endif
