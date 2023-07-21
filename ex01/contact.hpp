55ffc1353af601a6b9b06c9eb63a4447f8194dd7
abduraimoff21 <abduraimov21doston@gmail.com>
2023-07-22 03:39:11 +0400

    modified:   ex01/Makefile 	modified:   ex01/phonebook.cpp 	modified:   ex01/phonebook.hpp 	new file:   ex01/contact.cpp 	new file:   ex01/contact.hpp

--- ex01/contact.hpp
+++ ex01/contact.hpp
@@ -1,35 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*   contact.hpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/16 02:26:16 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/21 06:15:17 by dabdurai         ###   ########.fr       */
+/*   Created: 2023/07/21 01:15:59 by dabdurai          #+#    #+#             */
+/*   Updated: 2023/07/21 06:15:10 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHONENOOK_HPP
-# define PHONENOOK_HPP
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
 
 # include <iostream>
-# include <string>
 
-# define RED		"\033[31m"
-# define GREEN 	"\033[32m"
-# define YELLOW	"\033[33m"
-# define BLUE		"\033[34m"
-# define MAGENTA	"\033[35m"
-# define WHITE		"\033[37m"
 
-class Contact {
-	public:
-	std::string first_name;
-	std::string last_name;
-	std::string nick_name;
-	std::string darkest_secret;
-	std::string phone_number;
-};
 
 #endif
--- ex01/phonebook.cpp
+++ ex01/phonebook.cpp
@@ -6,17 +6,50 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:26:05 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/20 07:40:33 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/07/21 03:21:34 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
+#include "contact.hpp"
 
 int main () {
 	std::string command;
+	Contact myContact;
 	
-	std::cout << BLUE << "ENTER COMMANDS IN CAPITAL LETTERS" << std::endl;
+	std::cout << BLUE << "ENTER " << RED "ADD SEARCH EXIT" << BLUE << " COMMANDS IN CAPITAL LETTERS" << std::endl;
 	while (getline(std::cin, command)){
+		if (command == "ADD"){
+			std::cout << MAGENTA << "PLEASE FILL THE ALL FIELDS" << std::endl;
+			do {
+				std::cout << YELLOW << "ENTER THE FIRST NAME" << std::endl;
+				getline(std::cin, myContact.first_name);
+			} while (myContact.first_name.empty());
+			do {
+				std::cout << YELLOW << "ENTER THE LAST NAME" << std::endl;
+				getline(std::cin, myContact.last_name);
+			} while (myContact.last_name.empty());
+			do {
+				std::cout << YELLOW << "ENTER THE NICK NAME" << std::endl;
+				getline(std::cin, myContact.nick_name);
+			} while (myContact.nick_name.empty());
+			do {
+				std::cout << YELLOW << "ENTER THE PHONE NUMBER" << std::endl;
+				getline(std::cin, myContact.phone_number);
+			} while (myContact.phone_number.empty());
+			do {
+				std::cout << YELLOW << "ENTER THE DARKEST SECRET" << std::endl;
+				getline(std::cin, myContact.darkest_secret);
+			} while (myContact.darkest_secret.empty());
+		}
+		else if (command == "SEARCH"){
+			
+		}
+		else if (command == "EXIT"){
+			
+		}
+		else
+			std::cout << GREEN << "INVALID COMMAND. PLEASE ENTER THE VALID COMMAND" << std::endl;
 	}
 	return 0;
 }
