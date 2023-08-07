2f6660e4d1d8efd337178057dc22ce06a4ca4d83
abduraimoff21 <abduraimov21doston@gmail.com>
2023-08-07 14:27:57 +0400

    Final version

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:24:42 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/07/18 05:04:31 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/08/07 14:26:50 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,14 @@
 
 int main (int argc, char **argv){
 	int i;
-	int j;
 	char c;
 	if (argc > 1)
 	{
-		for (i = 1; argv[i]; i++)
+		std::string str = argv[i];
+		for (std::string::size_type j = 0; j < str.length(); ++j)
 		{
-			for (j = 0; argv[i][j]; j++)
-			{
-				c = std::toupper(argv[i][j]);	
-				std::cout << c;
-			}
+			c = std::toupper(str[j]);	
+			std::cout << c;
 		}
 		std::cout << "\n";
 	}
--- ex01/phonebook.hpp
+++ ex01/phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:26:16 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/08/06 20:15:10 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/08/07 14:26:22 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,14 @@ class Phonebook {
 		
 		void displayInform();
 		void storeContact(const Contact& contact);
-		void printContact(int index);
-		void print(int index);
 		void validIndex(int index);
-		std::string cutInfo(std::string temp);
 		
 	private:
+		std::string cutInfo(std::string temp);
+		
+		void printContact(int index);
+		void print(int index);
+
 		Contact contacts[8];
 		int numContacts;
 		int current_index;
