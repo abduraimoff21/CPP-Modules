efce3138891d4ecaff557e2954c1f9a9b08032b9
abduraimoff21 <abduraimov21doston@gmail.com>
2023-08-12 19:17:29 +0400

    modified:   ex00/megaphone.cpp

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 02:24:42 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/08/07 14:26:50 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/08/12 19:17:16 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ int main (int argc, char **argv){
 	char c;
 	if (argc > 1)
 	{
-		std::string str = argv[i];
-		for (std::string::size_type j = 0; j < str.length(); ++j)
-		{
-			c = std::toupper(str[j]);	
-			std::cout << c;
+		for(int i = 1; argv[i]; i++){
+			std::string str = argv[i];
+			for (std::string::size_type j = 0; j < str.length(); ++j)
+			{
+				c = std::toupper(str[j]);	
+				std::cout << c;
+			}
 		}
 		std::cout << "\n";
 	}
