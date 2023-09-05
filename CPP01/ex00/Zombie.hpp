544b0534f678e1d16d920d18da93244587ec6a90
abduraimoff21 <abduraimov21doston@gmail.com>
2023-09-05 19:39:16 +0400

    modified:   CPP01/ex00/Zombie.hpp

--- CPP01/ex00/Zombie.hpp
+++ CPP01/ex00/Zombie.hpp
@@ -6,7 +6,22 @@
 /*   By: dabdurai <dabdurai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:25:46 by dabdurai          #+#    #+#             */
-/*   Updated: 2023/09/04 17:25:47 by dabdurai         ###   ########.fr       */
+/*   Updated: 2023/09/04 19:41:11 by dabdurai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+
+class Zombie {
+    public:
+
+
+    private:
+        std::string Foo;
+    
+};
+
+#endif
